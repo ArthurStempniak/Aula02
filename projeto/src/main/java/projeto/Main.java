c630f66171c8085b56f746d87f6efda548921260
ArthurStempniak <arthurstempniak8@gmail.com>
2025-02-26 20:59:22 -0300

    teste

--- projeto/src/main/java/projeto/Main.java
+++ projeto/src/main/java/projeto/Main.java
@@ -5,12 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in); 
 
-        int escolha;
-
-        System.out.println("Bom dia meu amor!!" +
-                "\n1) Escolha");
-
-        escolha = scan.nextInt();
+        System.out.println("Bom Dia Piá");
 
         scan.close();
     }

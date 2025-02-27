fabc3ecb4c8269661f0a3666d5ab54631d2b7fac
ArthurStempniak <arthurstempniak8@gmail.com>
2025-02-26 21:07:20 -0300

    Olá Mundo

--- projeto/src/main/java/projeto/Main.java
+++ projeto/src/main/java/projeto/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in); 
 
-        System.out.println("Hola que tal");
+        System.out.println("Olá Mundo");
 
         scan.close();
     }
